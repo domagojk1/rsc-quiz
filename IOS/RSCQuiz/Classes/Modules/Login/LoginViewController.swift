5067bb83dc83aa0a3fd9863b47e5f01e11308761
Nikola Majcen <nikola.majcen@gmail.com>
2016-11-26 11:57:44 +0100

    Updated file structure

---
+++ IOS/RSCQuiz/Classes/Modules/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  RSCQuiz
+//
+//  Created by Nikola Majcen on 26/11/2016.
+//  Copyright © 2016 Nikola Majcen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Login screen")
+    }
+}
